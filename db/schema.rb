d7a7c4c5cc84f4a477496441d36ef7dc79e37565
Naokichi55 <ibosanganba@gmail.com>
2025-08-25 07:28:00 +0900

    add:docker compose exec web rails db;migrateを実施

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_08_24_085640) do
+ActiveRecord::Schema[7.2].define(version: 2025_08_24_222002) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.bigint "user_id"
+    t.bigint "racket_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["racket_id"], name: "index_comments_on_racket_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "rackets", force: :cascade do |t|
     t.string "product_name", null: false
@@ -45,5 +55,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_08_24_085640) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "rackets"
+  add_foreign_key "comments", "users"
   add_foreign_key "rackets", "users"
 end
