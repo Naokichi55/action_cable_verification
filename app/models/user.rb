bcc3eca374c94692c02b55186279657fd5f2f814
Naokichi55 <ibosanganba@gmail.com>
2025-08-25 07:08:52 +0900

    add:ラケット投稿機能を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :rackets, dependent: :destroy
+
+
+# 投稿者とログインユーザーが同じかを確認
+  def own?(object)
+    id == object&.user_id
+  end
 end
