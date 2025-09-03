1e6c055a4d93b67a1d20a4bfd2244ad5470f990f
Naokichi55 <ibosanganba@gmail.com>
2025-09-04 06:46:47 +0900

    fix:createアクション内のエラーを解決

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,9 @@
 class CommentsController < ApplicationController
   def create
-    comment = current_user.comments.build(comment_params)
-    if comment.save
-      ActionCable.server.broadcast 'comment_channel', content: comment, user: comment.user, date: comment.created_at.to_s(:datetime_jp), id: comment.id,post: comment.post
-      redirect_to racket_path(comment.racket), success: "コメント投稿に成功しました"
+    @comment = current_user.comments.build(comment_params)
+    if @comment.save
+      ActionCable.server.broadcast('comments_channel',{ body: @comment, date: @comment.created_at.strftime('%Y/%m/%d %H:%M'), user: @comment.user, id: @comment.id,racket: @comment.racket })
+      # redirect_to racket_path(comment.racket), success: "コメント投稿に成功しました"
     else
       redirect_to racket_path(comment.racket), danger: "コメント投稿に失敗しました"
     end
